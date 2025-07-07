1fb5bbbe5c33d86cf626e97ecaf75c56f2b198c1
Afdally <cikoh536@gmail.com>
2025-07-07 11:59:00 +0800

    perbaikan contract dan sidebar

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,15 @@ use App\Http\Controllers\MailController;
 Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/dashboard/filter', [DashboardController::class, 'getFilteredData'])->middleware(['auth', 'verified'])->name('dashboard.filter');
 
+// Email Manually
+Route::get('/email-manual', function () {
+    return view('email-manual');
+})->middleware(['auth', 'verified'])->name('email.manual');
+
 // Users
 Route::get('/users', function () {
     return view('users');
 })->middleware(['auth', 'verified'])->name('users.index');
-
-
-// Managers
-// Route::get('/managers', function () {
-//     return view('managers');
-// })->middleware(['auth', 'verified'])->name('managers');
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -35,9 +33,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
 Route::prefix('products')->middleware(['auth', 'verified'])->group(function() {
-
     Route::get('/', [ProdukController::class, 'index'])->name('products.index');
     Route::get('/create', [ProdukController::class, 'create'])->name('products.create');
     Route::post('/store', [ProdukController::class, 'store'])->name('products.store');
@@ -46,7 +42,6 @@ Route::prefix('products')->middleware(['auth', 'verified'])->group(function() {
     Route::delete('/delete/{id}', [ProdukController::class, 'destroy'])->name('products.destroy');
 });
 
-
 Route::prefix('customer')->middleware(['auth', 'verified'])->group(function() {
     Route::get('/', [CustomerController::class, 'index'])->name('customer.index');
     Route::get('/create', [CustomerController::class, 'create'])->name('customer.create');
@@ -54,7 +49,6 @@ Route::prefix('customer')->middleware(['auth', 'verified'])->group(function() {
     Route::get('/edit/{id}', [CustomerController::class, 'edit'])->name('customer.edit');
     Route::put('/{id}', [CustomerController::class, 'update'])->name('customer.update');
     Route::delete('/delete/{id}', [CustomerController::class, 'destroy'])->name('customer.destroy');
-    Route::post('/store-customer', [CustomerController::class, 'storeContract'])->name('customer.store_customer');
 });
 
 Route::prefix('managers')->middleware(['auth', 'verified'])->name('managers.')->group(function() {
@@ -81,6 +75,5 @@ Route::prefix('mail-manual')->middleware(['auth','verified'])->name('mail.')->gr
 Route::get('/user-dashboard', function () {
     return view('user-dashboard');
 })->middleware(['auth', 'verified'])->name('user.dashboard');
-
 
 require __DIR__.'/auth.php';
